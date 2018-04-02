8f2987edc238a295f2910ae5b6a3cbbfc4b9f092
Alex Ko <ko.e.alexander@gmail.com>
2018-04-01 23:05:05 -0400

    Added additional functionality

--- src/main/java/com/itextpdf/jumpstart/HelloWorld.java
+++ src/main/java/com/itextpdf/jumpstart/HelloWorld.java
@@ -1,9 +1,17 @@
 package com.itextpdf.jumpstart;
 
+import com.itextpdf.io.font.FontConstants;
+import com.itextpdf.io.image.ImageData;
+import com.itextpdf.io.image.ImageDataFactory;
+import com.itextpdf.kernel.color.Color;
+import com.itextpdf.kernel.font.PdfFontFactory;
 import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfPage;
 import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.kernel.pdf.canvas.PdfCanvas;
 import com.itextpdf.layout.Document;
 import com.itextpdf.layout.element.AreaBreak;
+import com.itextpdf.layout.element.Image;
 import com.itextpdf.layout.element.List;
 import com.itextpdf.layout.element.Paragraph;
 
@@ -12,6 +20,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 
 /*
+Project Structure -> module -> dependencies
 Requires:
 itext7 io jar
 itext7 kernal jar
@@ -20,6 +29,7 @@ slf4j api
 slf4j simple
 // https://stackoverflow.com/questions/12926899/java-lang-noclassdeffounderror-org-slf4j-loggerfactory
 // https://www.slf4j.org/download.html
+// https://www.tutorialspoint.com/itext/index.htm
  */
 
 public class HelloWorld {
@@ -44,9 +54,11 @@ public class HelloWorld {
         Document document = new Document(pdf);
 
         //Add paragraph to the content
-        document.add(new Paragraph("Hello World, this is Alex trying to learn programmatic pdf's!"));
+        // Change the font
+        document.add(new Paragraph("Hello World, this is Alex trying to learn programmatic pdf's!").setFont(PdfFontFactory.createFont(FontConstants.HELVETICA_BOLD)));
 
-        document.add(new Paragraph("This is a second paragraph!"));
+        // Change the color
+        document.add(new Paragraph("This is a second paragraph!").setFontColor(Color.BLUE));
 
         document.add(new Paragraph("This is the third paragraph! asdfasdf dasdadsfdskj;aolwehfjoaiheswf;olijaf;olaifjdjifdo;asdkjisf;oaijdef;oai j;alsdkjf;l kflkjasd;lfkj;asldkjf;lasjdf;lkjasd;flkjasd;lfkjasd;lfkj;lasdkjffjeo;awifjo;awifjdskif"));
 
@@ -59,7 +71,7 @@ public class HelloWorld {
         document.add(aB);
 
 
-
+        // Adding a list
         // Creating a list
         List list = new List();
 
@@ -75,6 +87,44 @@ public class HelloWorld {
 
         // Adding list to the document
         document.add(list);
+
+
+
+        // Adding an image
+        // Creating an ImageData object
+        String imFile = "C:/Users/Alex/Documents/itext7pdfdemo/intj.jpg";
+        ImageData data = ImageDataFactory.create(imFile);
+
+        // Creating an Image object
+        Image image = new Image(data);
+
+        // Setting the position of the image to the center of the page
+        image.setFixedPosition(100,250);
+
+        // Adding image to the document
+        document.add(image);
+
+
+
+
+        // Creating a line
+        // Creating a new page
+        PdfPage pdfPage = pdf.addNewPage();
+
+        // Creating a PdfCanvas object
+        PdfCanvas canvas = new PdfCanvas(pdfPage);
+
+        // Initial point of the line
+        canvas.moveTo(100, 300);
+
+        // Drawing the line
+        canvas.lineTo(500, 300);
+
+        // Closing the path stroke
+        canvas.closePathStroke();
+
+
+
 
         //Close document
         document.close();
